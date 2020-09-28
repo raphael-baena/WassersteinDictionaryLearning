956726190b1de00caf73264350f25035e10c9a7d
Baena Raphael <38126139+raphael-baena@users.noreply.github.com>
2020-09-28 14:00:02 +0200

    TF WDL updated (log-sinkhorn algorithm

--- python/WDL_tensorflow.py
+++ python/WDL_tensorflow.py
@@ -5,6 +5,9 @@ Created on Fri May 22 11:07:14 2020
 
 @author: raphaelbaena
 """
+
+
+#### WDL CODE ###
 import tensorflow as tf
 import multiprocessing as mp
 import numpy as np
@@ -120,11 +123,13 @@ class WDL:
         w = tf.cast(w,tf.float64)
         
         dicw0 = tf.reshape(log10(tf.concat([tf.transpose(Ys),w],axis=0)),[-1])
-        
+        #dicw0 = tf.concat([self.Datapoint[0],self.Datapoint[1]],axis=0)
+        #dicw0= tf.reshape(log10(tf.concat([tf.transpose(dicw0),w],axis =0)),[-1])
         #err ,fullgrad = self.LBFGSFunc(dicw0)
         #dic = tfp.optimizer.lbfgs_minimize(self.LBFGSFunc, initial_position = dicw0, max_iterations=15000,parallel_iterations=1)
-        dic= lbfgs(self.func, dicw0.numpy(),factr=10, pgtol=1e-10, maxiter=15000)
-        return   dic
+        dic= lbfgs(self.func, dicw0.numpy(),factr=10, pgtol=1e-10, maxiter=30)
+
+        return   dic, dicw0
     
 
  #x, f, dic = tfp.optimizer.lbfgs_minimize(self.LBFGSFunc, dicw0, max_iterations=self.maxiter)
@@ -163,6 +168,35 @@ class WDL:
   
         return  p
     @tf.function
+
+    def StabKer(self, alpha, beta):
+      #tf.expand_dims( tf.ones((10,20)) ,axis=-1)
+#tf.expand_dims( tf.ones((10,20)) ,axis=1)
+#tf.expand_dims( tf.ones((10,20)) ,axis=0)
+        M = tf.expand_dims(-self.Cost,axis = -1) + tf.expand_dims(alpha, axis = 1) + tf.expand_dims(beta, axis = 0) 
+        M = tf.math.exp(M / self.gamma)
+        return M
+
+# Log Sinkhorn iteration
+    def log_sinkhorn_step(self,i,alpha, beta, logp, logD, lbda, Epsilon=1e-100):
+          M = self.StabKer(alpha,beta)
+          #M = tf.clip_by_value(M, clip_value_min=tf.cast(Epsilon,dtype=tf.float64), clip_value_max=1e7)
+          
+          newalpha = self.gamma * (logD - tf.math.log(tf.math.reduce_sum(M,axis=1) + Epsilon)) + alpha
+        
+          alpha = self.tau*alpha + (1.-self.tau)*newalpha
+          
+          M = self.StabKer(alpha,beta)
+          #M = tf.clip_by_value(M, clip_value_min=tf.cast(Epsilon,dtype=tf.float64), clip_value_max=1e7)
+          lKta = tf.math.log(tf.math.reduce_sum(M, axis=0) + Epsilon) - beta/self.gamma
+          logp = tf.math.reduce_sum(lbda*lKta, axis=1)
+          newbeta = self.gamma * (tf.expand_dims(logp,-1) - lKta)
+
+          beta = self.tau*beta + (1.-self.tau)*newbeta
+          return i+1,alpha, beta, logp ,logD, lbda
+
+
+    @tf.function
     def sinkhorn_step(self,i,a,b,p,Newvar_D,Newvar_lbda):
       newa = Newvar_D/tf.matmul(self.Ker,b)
       a = a**self.tau * newa**(1.-self.tau)
@@ -189,15 +223,18 @@ class WDL:
       a,b,p =tf.ones_like(Newvar_D),tf.ones_like(Newvar_D),tf.ones_like(Newvar_D[:,0])
       i= 0
       def return_False(i,a ,b,p,Newvar_D, Newvar_lbda):
-          return i<100
-      i,a,b,p,Newvar_D, Newvar_lbda=tf.while_loop(return_False,self.unbal_sinkhorn_step,loop_vars=[i,a,b,p,Newvar_D, Newvar_lbda])
+          return i<self.n_iter_sink
+      
+      i,a,b,p,Newvar_D, Newvar_lbda= tf.while_loop(return_False,self.log_sinkhorn_step,loop_vars=[i,a,b,p,tf.math.log(Newvar_D), Newvar_lbda])
+      #tf.while_loop(return_False,self.unbal_sinkhorn_step,loop_vars=[i,a,b,p,Newvar_D, Newvar_lbda])
       return p
 
     def Loss(self,Newvar_D, Newvar_lbda):
       arg=Newvar_D, Newvar_lbda
       p =  self.wass_grad(arg)
-      p=p/tf.math.reduce_sum(p)
-      return tf.norm( self.datapoint-p, ord='euclidean')**2*1/2 #tf.math.reduce_sum(p*tf.math.log(p/self.datapoint - p + self.datapoint))#tf.math.reduce_sum((self.datapoint-p)**2)*1/2
+      p=tf.math.exp(p)
+      #p=p/tf.math.reduce_sum(p)
+      return tf.norm( self.datapoint-p, ord='euclidean')**2*1/2*1000 #tf.math.reduce_sum(p*tf.math.log(p/self.datapoint - p + self.datapoint))#tf.math.reduce_sum((self.datapoint-p)**2)*1/2
 
     def varchange_wass_grad(self,arg):
         self.datapoint,wi = arg
@@ -238,22 +275,11 @@ class WDL:
       return [vv.numpy().astype(np.float64)  for vv in self.LBFGSFunc(tf.constant(x, dtype=tf.float32))]
 
 
-    #err =tf.math.reduce_sum( this_err)
-    #, fullgrad.flatten()
-
-# for i in range(n):
-
-#                     this_err, grad, graw = self.varchange_Theano_wass_grad((self.Datapoint[i],Ys,w[i]))
-#                     err+=this_err
-
-#                     fullgrad_part_1   = fullgrad_part_1 + tf.cast(grad/n,tf.float64)
-
-#                     fullgrad_part_2.append( tf.cast(graw,dtype=tf.float64))
 @tf.function
 def log10(x):
   numerator = tf.math.log(x)
   denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
-  return numerator / denominator
+  return numerator #/ denominator
 
 
 @tf.function
